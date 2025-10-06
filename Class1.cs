b8e7aa71c019194bd4efef267966e0a637f72543
Tayyba250 <bopbank12@gmail.com>
2025-10-06 21:29:22 +0500

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class EvenOrOddChecker
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+            Console.WriteLine($"{number} is Even.");
+        else
+            Console.WriteLine($"{number} is Odd.");
+    }
+}
