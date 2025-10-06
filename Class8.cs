7596b5ed04e38476a45e0f3caf49a04beda151fa
Tayyba250 <bopbank12@gmail.com>
2025-10-06 21:40:05 +0500

    Create Class8.cs

---
+++ Class8.cs
@@ -0,0 +1,30 @@
+using System;
+
+class ArrayMaxMin
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        Console.WriteLine("Enter 10 integers:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Number {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (numbers[i] > max)
+                max = numbers[i];
+            if (numbers[i] < min)
+                min = numbers[i];
+        }
+
+        Console.WriteLine($"Maximum = {max}");
+        Console.WriteLine($"Minimum = {min}");
+    }
+}
