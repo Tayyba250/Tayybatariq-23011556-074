1a4f4c31904f90c3aeb25c25597a72caa95e78ce
Tayyba250 <bopbank12@gmail.com>
2025-10-06 21:39:15 +0500

    Create Class7.cs

---
+++ Class7.cs
@@ -0,0 +1,20 @@
+using System;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to reverse: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversed = 0;
+        while (number != 0)
+        {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Reversed number = {reversed}");
+    }
+}
